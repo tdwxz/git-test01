7273a461187b784367d3b419543a5cbe23930d15
tdw <2039906074@qq.com>
2022-01-03 15:23:51 +0800

    v1.3 hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub1");
+        System.out.println("Hello GitHub2");
+        System.out.println("Hello GitHub3");
     }
 }
